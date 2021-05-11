adc300687b7813f0e040673ca5af4310551ccdcf
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-11 10:45:16 -0300

    iniciando projeto

---
+++ src/main/java/com/fillipe/testeactglobo/TesteActGloboApplication.java
@@ -0,0 +1,13 @@
+package com.fillipe.testeactglobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteActGloboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteActGloboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fillipe/testeactglobo/TesteActGloboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fillipe.testeactglobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteActGloboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
